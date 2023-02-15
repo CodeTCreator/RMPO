e3405486f50fda45ff5e797d24cf042834d95cdc
Bogdan <modernstrategist@gmail.com>
2023-02-15 10:55:56 +0300

    Stage 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view) {
+        TextView textView = findViewById(R.id.textView_1);
+        EditText editText = findViewById(R.id.editText_1);
+        textView.setText("Вы вввели:" + editText.getText());
+    }
+    public void Reset_value(View view){
+        TextView textView = findViewById(R.id.textView_1);
+        EditText editText = findViewById(R.id.editText_1);
+        textView.setText(R.string.data_view_1);
+        editText.getText().clear();
     }
 }

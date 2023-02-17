be11739316272139b20cd57c31e2f632bf84ae77
Bogdan <modernstrategist@gmail.com>
2023-02-17 22:18:13 +0300

    LW 2. OptionsMenu, меняем цвет фона.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,53 +1,79 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.SeekBar;
+import android.text.Layout;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Menu;
+import android.view.SubMenu;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout my_layout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        SeekBar seekBar = findViewById(R.id.SeekBar_1);
-        TextView textView = findViewById(R.id.Seek_value);
-        TextView textView1 = findViewById(R.id.textView_1);
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-                textView.setText(String.valueOf(progress));
-                textView1.setTextSize(progress);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-
+        my_layout = findViewById(R.id.layout_1);
     }
-    public void sendMessage(View view) {
-        TextView textView = findViewById(R.id.textView_1);
-        EditText editText = findViewById(R.id.editText_1);
-        textView.setText("Вы вввели:" + editText.getText());
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return true;
     }
-    public void Reset_value(View view){
-        TextView textView = findViewById(R.id.textView_1);
-        EditText editText = findViewById(R.id.editText_1);
-        textView.setText(R.string.data_view_1);
-        editText.getText().clear();
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        TextView view = findViewById(R.id.textView_1);
+        int b = 5;
+        int[] colors = new int[b];
+        colors[0]= R.color.red;
+        colors[1]= R.color.purple_700;
+        colors[2]= R.color.black;
+        colors[3]= R.color.green;
+        colors[4]= R.color.white;
+        Resources resources = getResources();
+        switch (id){
+            case R.id.item1:
+                view.setText("Выбор цвета");
+                return true;
+            case R.id.item2:
+                view.setText("Настройки");
+                return true;
+            case R.id.item3:
+                view.setText("Сброс до заводских");
+
+                return true;
+            case R.id.item1_1 :
+                my_layout.setBackgroundColor(resources.getColor(colors[0],  null));
+                return true;
+            case R.id.item1_2:
+                my_layout.setBackgroundColor(resources.getColor(colors[1],  null));
+                return true;
+            case R.id.item1_3:
+                my_layout.setBackgroundColor(resources.getColor(colors[2],  null));
+                return true;
+            case R.id.item1_4:
+                my_layout.setBackgroundColor(resources.getColor(colors[3],  null));
+                return true;
+            case R.id.item1_5:
+                int random_number1 = 1 + (int) (Math.random() * b);
+                my_layout.setBackgroundColor(resources.getColor(colors[random_number1],  null));
+                return true;
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
